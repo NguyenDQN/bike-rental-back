8487cf55cf3dc76c2eac094a75948663b440a345
NguyenDQN <nguyencn11@gmai.com>
2023-10-27 16:58:17 +0700

    update Bike controller

--- src/main/java/com/nguyendq/bikerentalback/controllers/BikesController.java
+++ src/main/java/com/nguyendq/bikerentalback/controllers/BikesController.java
@@ -17,7 +17,7 @@ public class BikesController {
         Bike bike2 = Bike.builder().id(2L).name("Air blade").price(150f).build();
         Bike bike3 = Bike.builder().id(3L).name("Exciter").price(150f).build();
         Bike bike4 = Bike.builder().id(4L).name("Vision").price(150f).build();
-        Bike bike5 = Bike.builder().id(5L).name("Blade").price(150f).build();
+        Bike bike5 = Bike.builder().id(5L).name("Blade modified").price(150f).build();
         return Arrays.asList(bike, bike2, bike3, bike4, bike5);
     }
 }
