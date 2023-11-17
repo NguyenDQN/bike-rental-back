b495972b2b66a5708b2a189f77d02b7287f31827
NguyenDQN <nguyencn11@gmai.com>
2023-11-17 17:24:37 +0700

    Add deployment for k8s

---
+++ README.md
@@ -0,0 +1,15 @@
+To build a new jar: ./gradlew build
+TO build docker image:
+    cd to the folder contains DockerFiles
+    docker build -t bike-rental-back:v0.0.1 .
+To run container on docker:
+    docker run -p 8080:80 bike-rental-back-v4
+    8080: port on local machine
+    80: exposed port on docker
+
+- Start minikubec: `minikube start` (incase it does not work start with--insecure-registry)
+- In the new terminal, run `eval $(minikube docker-env)`
+- Build image `docker build . -t bike-rental-back`
+- Run `kubectl apply -f k8s_deployment.yaml` to create a deployment
+- Expose deployment as service, port of service must be the same as port exposed port of the image
+Note: build a new jar -> build docker image -> update image in yaml file -> apply to kubectrl
--- src/main/java/com/nguyendq/bikerentalback/controllers/BikesController.java
+++ src/main/java/com/nguyendq/bikerentalback/controllers/BikesController.java
@@ -1,23 +1,27 @@
 package com.nguyendq.bikerentalback.controllers;
 
 import com.nguyendq.bikerentalback.models.Bike;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class BikesController {
     @CrossOrigin(origins = "*")
     @GetMapping("/bikes")
-    public List<Bike> getBikes() {
+    public List<Bike> getBikes() throws Exception {
+        log.info("Getting bikes from server: {}", InetAddress.getLocalHost().getHostName());
         Bike bike = Bike.builder().id(1L).name("Wave @").price(100f).build();
         Bike bike2 = Bike.builder().id(2L).name("Air blade").price(150f).build();
         Bike bike3 = Bike.builder().id(3L).name("Exciter").price(150f).build();
         Bike bike4 = Bike.builder().id(4L).name("Vision").price(150f).build();
-        Bike bike5 = Bike.builder().id(5L).name("Blade modified").price(150f).build();
+        Bike bike5 = Bike.builder().id(5L).name("Blade v2").price(150f).build();
         return Arrays.asList(bike, bike2, bike3, bike4, bike5);
     }
 }
